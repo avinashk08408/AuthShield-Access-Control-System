c54d8aec2afe85197cb8ad25efeafeaef4750c03
avinashk08408 <avinashk08408@gmail.com>
2026-04-01 21:28:18 +0530

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,13 @@
 const CACHE = "authshield-v1";
+
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(["/login"])));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(["/login"]))
+  );
 });
+
 self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
